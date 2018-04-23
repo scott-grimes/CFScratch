311c81dbf4409932a1af5968323b9c44a5084ad5
Scott Grimes <scott-grimes@users.noreply.github.com>
2018-04-22 21:52:05 -0500

    Update customComponents.js

--- Hardware/customComponents.js
+++ Hardware/customComponents.js
@@ -112,6 +112,79 @@
     };
   });
 
+
+  $s.registerDevice('CUSTOMBUSOUT', function(device) {
+    var numInputs = Math.max(2, device.deviceDef.numInputs || 16);
+    device.halfPitch = true;
+    device.deviceDef['isBus'] = true;
+
+    var max = Math.pow(2,numInputs-1)-1;
+
+        device.deviceDef['value']= 0;    
+
+    if(!device.deviceDef['value'])
+        device.deviceDef['value']= 0;
+    else if(device.deviceDef['value']>max)
+      device.deviceDef['value']= max;
+
+
+    var dec2bin = function(int) {
+      var u = new Uint32Array(1);
+      var nbit = 16;
+      u[0] = int;
+      int = Math.pow(2, 16) - 1;
+      var converted = u[0] & int;
+      converted.toString(2);
+      return converted.toString(2).padStart(16,"0")
+    }
+
+    var $textInput = $s.createSVGElement('text');
+
+    var pushBusValue = function(){
+      var busValue = [];
+      
+      let binvalue = dec2bin(device.deviceDef['value']);
+      for (var i = numInputs-1; i >=0; i -= 1) {
+        let value = binvalue.charAt(i);
+
+        busValue.push( value==='1' ? 1 : null );
+      }
+
+      device.getOutputs()[0].setValue(
+          busValue);
+      $textInput.text(device.deviceDef['value'])
+    }
+
+    device.addOutput('', 'x' + numInputs);
+    pushBusValue();
+    
+    device.$ui.on('inputValueChange', function() {
+      pushBusValue();
+    });
+    var super_createUI = device.createUI;
+    device.createUI = function() {
+      super_createUI();
+      var size = device.getSize();
+        var w = size.width;
+        var h = size.height;
+          $textInput.
+          text(device.deviceDef['value']).
+          css('font-size', 12 + 'px').
+          attr('class', 'simcir-device-label').
+          attr({x: w / 2, y: h/2 });
+          
+        device.$ui.append($textInput);
+
+      device.doc = {
+        params: [
+          {name: 'numInputs', type: 'number', defaultValue: 16,
+            description: 'number of inputs.'}
+        ],
+        code: '{"type":"' + device.deviceDef.type + '","numInputs":8}'
+      };
+    };
+  });
+
     
  var createCustomInputFactory = function(type) {
     return function(device) {
@@ -185,11 +258,214 @@
         device.$ui.addClass('simcir-basicset-switch');
       };
     };
-};
+  };
     
-    $s.registerDevice('SINGLEINPUT', createCustomInputFactory('Toggle') );
+  $s.registerDevice('SINGLEINPUT', createCustomInputFactory('Toggle') );
 
+
+
+
+
+  $s.registerDevice('CUSTOMBUSIN', function(device) {
+
+    var numOutputs = Math.max(2, device.deviceDef.numOutputs || 16);
+    device.halfPitch = true;
+    device.deviceDef['isBus'] = true;
+    device.deviceDef['value']= 0;
+    device.addInput('', 'x' + numOutputs);
+    var $textDisplay = $s.createSVGElement('text');
+    device.$ui.on('inputValueChange', function() {
+      var busValue = device.getInputs()[0].getValue();
+        device.deviceDef['value'] = 0;
+      if(busValue!= null && typeof busValue == 'object'){
+        let pow = 0;
+        let sum = 0;
+        for(let i = 0;i<busValue.length;i++){
+          if(busValue[i]===1){
+            sum+=Math.pow(2,pow)
+          }
+          else if (busValue[i]===null){
+
+          }
+          else{ throw('bus not build correctly')}
+          pow+=1;
+        }
+      device.deviceDef['value'] = sum;
+      }
+      $textDisplay.text(device.deviceDef['value'])        
     
-  
+    });
+
+    var super_createUI = device.createUI;
+    device.createUI = function() {
+        super_createUI();
+        var size = device.getSize();
+        var w = size.width;
+        var h = size.height;
+          $textDisplay.
+          text(device.deviceDef['value']).
+          css('font-size', 12 + 'px').
+          attr('class', 'simcir-device-label').
+          attr({x: w / 2, y: h/2 });
+          
+        device.$ui.append($textDisplay);
+
+        device.doc = {
+        params: [
+          {name: 'numOutputs', type: 'number', defaultValue: 16,
+            description: 'number of outputs.'}
+        ],
+        code: '{"type":"' + device.deviceDef.type + '","numOutputs":8}'
+      };
+    }
+    
+    
+      
+    });
+
+
+
+let muxdata = {
+  "width":600,
+  "height":400,
+  "showToolbox":false,
+  "devices":[
+    {"type":"NOT","id":"dev0","x":150,"y":250,"label":"NOT"},
+    {"type":"AND","id":"dev1","x":150,"y":150,"label":"AND"},
+    {"type":"OR","id":"dev2","x":250,"y":150,"label":"OR"},
+    {"type":"AND","id":"dev3","x":200,"y":50,"label":"AND"},
+    {"type":"Out","id":"dev4","x":344,"y":144,"label":"OUT"},
+    {"type":"In","id":"dev5","x":48,"y":56,"label":"A"},
+    {"type":"In","id":"dev6","x":40,"y":160,"label":"B"},
+    {"type":"In","id":"dev7","x":48,"y":256,"label":"SEL"}
+  ],
+  "connectors":[
+    {"from":"dev0.in0","to":"dev7.out0"},
+    {"from":"dev1.in0","to":"dev6.out0"},
+    {"from":"dev1.in1","to":"dev7.out0"},
+    {"from":"dev2.in0","to":"dev3.out0"},
+    {"from":"dev2.in1","to":"dev1.out0"},
+    {"from":"dev3.in0","to":"dev5.out0"},
+    {"from":"dev3.in1","to":"dev0.out0"},
+    {"from":"dev4.in0","to":"dev2.out0"}
+  ],
+    "layout":{"rows":6,"cols":8,"hideLabelOnWorkspace":false,
+    "nodes":{"A":"L1","B":"L3","SEL":"L5","OUT":"R3"}}
+
+}
+$s.registerDevice('MUX',muxdata);
+
+let mux16data = {
+
+  "width":600,
+  "height":400,
+  "showToolbox":false,
+
+
+       "devices":[
+    {"type":"In","immobile":true,"isBus":true,"id":"dev0","x":48,"y":40,"label":"A"},
+    {"type":"BusIn","numOutputs":16,"immobile":true,"id":"dev1","x":168,"y":16,"label":"BusIn"},
+    {"type":"In","immobile":true,"isBus":true,"id":"dev2","x":48,"y":232,"label":"B"},
+    {"type":"BusIn","numOutputs":16,"immobile":true,"id":"dev3","x":168,"y":184,"label":"BusIn"},
+    {"type":"Out","immobile":true,"isBus":true,"id":"dev4","x":650,"y":144,"label":"OUT"},
+    {"type":"BusOut","numInputs":16,"immobile":true,"id":"dev5","x":536,"y":88,"label":"BusOut"},
+    {"type":"In","immobile":true,"id":"dev6","x":48,"y":328,"label":"SEL"},
+    {"type":"MUX","id":"dev7","x":336,"y":0,"label":"MUX"},
+    {"type":"MUX","id":"dev8","x":336,"y":100,"label":"MUX"},
+    {"type":"MUX","id":"dev9","x":336,"y":200,"label":"MUX"},
+    {"type":"MUX","id":"dev10","x":336,"y":300,"label":"MUX"},
+    {"type":"MUX","id":"dev11","x":336,"y":400,"label":"MUX"},
+    {"type":"MUX","id":"dev12","x":336,"y":500,"label":"MUX"},
+    {"type":"MUX","id":"dev13","x":336,"y":600,"label":"MUX"},
+    {"type":"MUX","id":"dev14","x":336,"y":700,"label":"MUX"},
+    {"type":"MUX","id":"dev15","x":336,"y":800,"label":"MUX"},
+    {"type":"MUX","id":"dev16","x":336,"y":900,"label":"MUX"},
+    {"type":"MUX","id":"dev17","x":336,"y":1000,"label":"MUX"},
+    {"type":"MUX","id":"dev18","x":336,"y":1100,"label":"MUX"},
+    {"type":"MUX","id":"dev19","x":336,"y":1200,"label":"MUX"},
+    {"type":"MUX","id":"dev20","x":336,"y":1300,"label":"MUX"},
+    {"type":"MUX","id":"dev21","x":336,"y":1400,"label":"MUX"},
+    {"type":"MUX","id":"dev22","x":336,"y":1500,"label":"MUX"}
+  ],
+  "connectors":[
+    {"from":"dev1.in0","to":"dev0.out0"},
+    {"from":"dev3.in0","to":"dev2.out0"},
+    {"from":"dev4.in0","to":"dev5.out0"},
+    {"from":"dev5.in0","to":"dev7.out0"},
+    {"from":"dev5.in1","to":"dev8.out0"},
+    {"from":"dev5.in2","to":"dev9.out0"},
+    {"from":"dev5.in3","to":"dev10.out0"},
+    {"from":"dev5.in4","to":"dev11.out0"},
+    {"from":"dev5.in5","to":"dev12.out0"},
+    {"from":"dev5.in6","to":"dev13.out0"},
+    {"from":"dev5.in7","to":"dev14.out0"},
+    {"from":"dev5.in8","to":"dev15.out0"},
+    {"from":"dev5.in9","to":"dev16.out0"},
+    {"from":"dev5.in10","to":"dev17.out0"},
+    {"from":"dev5.in11","to":"dev18.out0"},
+    {"from":"dev5.in12","to":"dev19.out0"},
+    {"from":"dev5.in13","to":"dev20.out0"},
+    {"from":"dev5.in14","to":"dev21.out0"},
+    {"from":"dev5.in15","to":"dev22.out0"},
+    {"from":"dev7.in0","to":"dev1.out0"},
+    {"from":"dev7.in1","to":"dev3.out0"},
+    {"from":"dev7.in2","to":"dev6.out0"},
+    {"from":"dev8.in0","to":"dev1.out1"},
+    {"from":"dev8.in1","to":"dev3.out1"},
+    {"from":"dev8.in2","to":"dev6.out0"},
+    {"from":"dev9.in0","to":"dev1.out2"},
+    {"from":"dev9.in1","to":"dev3.out2"},
+    {"from":"dev9.in2","to":"dev6.out0"},
+    {"from":"dev10.in0","to":"dev1.out3"},
+    {"from":"dev10.in1","to":"dev3.out3"},
+    {"from":"dev10.in2","to":"dev6.out0"},
+    {"from":"dev11.in0","to":"dev1.out4"},
+    {"from":"dev11.in1","to":"dev3.out4"},
+    {"from":"dev11.in2","to":"dev6.out0"},
+    {"from":"dev12.in0","to":"dev1.out5"},
+    {"from":"dev12.in1","to":"dev3.out5"},
+    {"from":"dev12.in2","to":"dev6.out0"},
+    {"from":"dev13.in0","to":"dev1.out6"},
+    {"from":"dev13.in1","to":"dev3.out6"},
+    {"from":"dev13.in2","to":"dev6.out0"},
+    {"from":"dev14.in0","to":"dev1.out7"},
+    {"from":"dev14.in1","to":"dev3.out7"},
+    {"from":"dev14.in2","to":"dev6.out0"},
+    {"from":"dev15.in0","to":"dev1.out8"},
+    {"from":"dev15.in1","to":"dev3.out8"},
+    {"from":"dev15.in2","to":"dev6.out0"},
+    {"from":"dev16.in0","to":"dev1.out9"},
+    {"from":"dev16.in1","to":"dev3.out9"},
+    {"from":"dev16.in2","to":"dev6.out0"},
+    {"from":"dev17.in0","to":"dev1.out10"},
+    {"from":"dev17.in1","to":"dev3.out10"},
+    {"from":"dev17.in2","to":"dev6.out0"},
+    {"from":"dev18.in0","to":"dev1.out11"},
+    {"from":"dev18.in1","to":"dev3.out11"},
+    {"from":"dev18.in2","to":"dev6.out0"},
+    {"from":"dev19.in0","to":"dev1.out12"},
+    {"from":"dev19.in1","to":"dev3.out12"},
+    {"from":"dev19.in2","to":"dev6.out0"},
+    {"from":"dev20.in0","to":"dev1.out13"},
+    {"from":"dev20.in1","to":"dev3.out13"},
+    {"from":"dev20.in2","to":"dev6.out0"},
+    {"from":"dev21.in0","to":"dev1.out14"},
+    {"from":"dev21.in1","to":"dev3.out14"},
+    {"from":"dev21.in2","to":"dev6.out0"},
+    {"from":"dev22.in0","to":"dev1.out15"},
+    {"from":"dev22.in1","to":"dev3.out15"},
+    {"from":"dev22.in2","to":"dev6.out0"}
+  ]
+}
+$s.registerDevice('MUX16',mux16data);
+
+
+
+
+
 
 }(simcir);
+
+
+
+  
