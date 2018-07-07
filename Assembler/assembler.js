500bd2e80f4f39125e467a1a747ba7993d47f258
Scott <scottgrimes89@gmail.com>
2018-07-06 19:32:07 -0500

    Pretty

--- Assembler/assembler.js
+++ Assembler/assembler.js
@@ -4,7 +4,7 @@ class Assembler {
   constructor() {
   }
 
-  process(text){
+  process(text) { 
          
     this.buildDicts();
     this.machineCode = []; // to be filled with the assembled machine code commands
@@ -24,254 +24,253 @@ class Assembler {
   buildDicts() {
     // Built-In Symbols which may be used
     this.symbolDict = {
-    "SP": 0,
-    "LCL": 1,
-    "ARG": 2,
-    "THIS": 3,
-    "THAT": 4,
-    "SCREEN": 16384,
-    "KBD": 24576,
-    "R0": 0,
-    "R1": 1,
-    "R2": 2,
-    "R3": 3,
-    "R4": 4,
-    "R5": 5,
-    "R6": 6,
-    "R7": 7,
-    "R8": 8,
-    "R9": 9,
-    "R10": 10,
-    "R11": 11,
-    "R12": 12,
-    "R13": 13,
-    "R14": 14,
-    "R15": 15
+      'SP': 0,
+      'LCL': 1,
+      'ARG': 2,
+      'THIS': 3,
+      'THAT': 4,
+      'SCREEN': 16384,
+      'KBD': 24576,
+      'R0': 0,
+      'R1': 1,
+      'R2': 2,
+      'R3': 3,
+      'R4': 4,
+      'R5': 5,
+      'R6': 6,
+      'R7': 7,
+      'R8': 8,
+      'R9': 9,
+      'R10': 10,
+      'R11': 11,
+      'R12': 12,
+      'R13': 13,
+      'R14': 14,
+      'R15': 15
     };
         
     this.cmd = {
-        '0': '0101010',
-        '1': '0111111',
-        '-1': '0111010',
-        'D' : '0001100',
-        'A' : '0110000',
-        '!D' : '0001101',
-        '!A' : '0110001',
-        '-D' : '0001111',
-        '-A' : '0110011',
-        'D+1': '0011111',
-        'A+1': '0110111',
-        'D-1': '0001110',
-        'A-1': '0110010',
-        'D+A' : '0000010',
-        'D-A' : '0010011',
-        'A-D' : '0000111',
-        'D&A' : '0000000',
-        'D|A' : '0010101',
-        'M' : '1110000',
-        '!M' : '1110001',
-        '-M' : '1110011',
-        'M+1': '1110111',
-        'M-1': '1110010',
-        'D+M' : '1000010',
-        'D-M' : '1010011',
-        'M-D' : '1000111',
-        'D&M' : '1000000',
-        'D|M' : '1010101' };
+      '0': '0101010',
+      '1': '0111111',
+      '-1': '0111010',
+      'D': '0001100',
+      'A': '0110000',
+      '!D': '0001101',
+      '!A': '0110001',
+      '-D': '0001111',
+      '-A': '0110011',
+      'D+1': '0011111',
+      'A+1': '0110111',
+      'D-1': '0001110',
+      'A-1': '0110010',
+      'D+A': '0000010',
+      'D-A': '0010011',
+      'A-D': '0000111',
+      'D&A': '0000000',
+      'D|A': '0010101',
+      'M': '1110000',
+      '!M': '1110001',
+      '-M': '1110011',
+      'M+1': '1110111',
+      'M-1': '1110010',
+      'D+M': '1000010',
+      'D-M': '1010011',
+      'M-D': '1000111',
+      'D&M': '1000000',
+      'D|M': '1010101' };
     
-    this.jmp = {'JGT' : "001",
-                'JEQ' : "010",
-                'JGE' : "011",
-                'JLT' : "100",
-                'JNE' : "101",
-                'JLE' : "110",
-                'JMP' : "111"};
+    this.jmp = {'JGT': '001',
+      'JEQ': '010',
+      'JGE': '011',
+      'JLT': '100',
+      'JNE': '101',
+      'JLE': '110',
+      'JMP': '111'};
     
     this.dst = { 
-        '0' : '000',
-        'M' : '001',
-        'D'  : '010',
-        'MD'  : '011',
-        'A'  : '100',
-        'AM'  : '101',
-        'AD'  : '110',
-        'AMD' : '111'};
+      '0': '000',
+      'M': '001',
+      'D': '010',
+      'MD': '011',
+      'A': '100',
+      'AM': '101',
+      'AD': '110',
+      'AMD': '111'};
     
-    this.validChars = 'abcdefghijklmnopqrstuvwxyz'
-    this.validChars+= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    this.validChars+= '0123456789'
-    this.validChars+= '@_+-.:!&|$;=()'
+    this.validChars = 'abcdefghijklmnopqrstuvwxyz';
+    this.validChars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.validChars += '0123456789';
+    this.validChars += '@_+-.:!&|$;=()';
     
-    this.userDefVarsCount = 0
-}
+    this.userDefVarsCount = 0;
+  }
 
 
-// this does shit with the labels
-parseLabels() {
+  // this does shit with the labels
+  parseLabels() {
     var lineNumber = 0;
-    for(var i = 0;i<this.lines.length;i++) {
-        if (this.lines[i][0].charAt(0) ==='(') {
-            if(this.lines[i][0].slice(-1)!==')') {
-                throw('Line: '+this.lines[i][1]+' incorrect label format: '+this.lines[i][0])
-            }
-            var label = this.lines[i][0].slice(1,this.lines[i].length-1);
-            this.symbolDict[label] = lineNumber;
-        }else {
-            lineNumber+=1;
+    for (var i = 0; i < this.lines.length; i++) {
+      if (this.lines[i][0].charAt(0) === '(') {
+        if (this.lines[i][0].slice(-1) !== ')') {
+          throw ('Line: ' + this.lines[i][1] + ' incorrect label format: ' + this.lines[i][0]);
         }
+        var label = this.lines[i][0].slice(1, this.lines[i].length - 1);
+        this.symbolDict[label] = lineNumber;
+      } else {
+        lineNumber += 1;
+      }
     }
-}
+  }
 
-/* 
+  /* 
 *   Fills this.lines with the given text, after whitespace, comments, and blank lines are removed.
 *   If any invalid characters are found, throws an exception
 */
-read(text) {
+  read(text) {
     var lines = text.split('\n');
     // loops through each line
-    for(var i = 0;i<lines.length;i++){
+    for (var i = 0; i < lines.length; i++) {
         
-        //removes any whitespace
-        lines[i] = lines[i].replace(/\s/g, "");
+      //removes any whitespace
+      lines[i] = lines[i].replace(/\s/g, '');
         
-        //removes any comments from the line
-        lines[i] = lines[i].split('//')[0];
+      //removes any comments from the line
+      lines[i] = lines[i].split('//')[0];
         
-        //checks for invalid characters
-        for(var j = 0;j<lines[i].length;j++) {
-            if(this.validChars.indexOf( lines[i].charAt(j) ) === -1) {
-                throw('Line: '+(i+1)+' invalid expression or character: ', lines[i]);
-            }
+      //checks for invalid characters
+      for (var j = 0; j < lines[i].length; j++) {
+        if (this.validChars.indexOf( lines[i].charAt(j) ) === -1) {
+          throw ('Line: ' + (i + 1) + ' invalid expression or character: ', lines[i]);
         }
+      }
         
-        //adds the parsed line to our list of lines
-        if(lines[i]!=='')
-            this.lines.push([lines[i],(i+1)]);
+      //adds the parsed line to our list of lines
+      if (lines[i] !== '') { this.lines.push([lines[i], (i + 1)]); }
         
     }
     
-}
+  }
 
                         
-//builds the machine code command associated with the given line                
-buildLine(linearr){
-    var line = linearr[0]
+  //builds the machine code command associated with the given line                
+  buildLine(linearr) {
+    var line = linearr[0];
     //no blank lines should be encountered at this point
-    if(line === '') 
-        throw('blank line encountered');
+    if (line === '') {
+      throw ('blank line encountered');
+    }
 
     //a command can be a label (, address @, or computation C
     var commandType = this.commandType(line);
     
     // ignore label lines
-    if(commandType==='L') 
-        return;
+    if (commandType === 'L') { return; }
     
     var command;
     
-    if(commandType==='A'){
-        command = this.aCommand(linearr)
-    }else if(commandType==='C'){
-        command = this.cCommand(linearr)
-    }
-    else{
-        throw('Line: '+linearr[1]+', invalid command',line);
+    if (commandType === 'A') {
+      command = this.aCommand(linearr);
+    } else if (commandType === 'C') {
+      command = this.cCommand(linearr);
+    } else {
+      throw ('Line: ' + linearr[1] + ', invalid command', line);
     }
         
     this.machineCode.push(command);
 
-    }
+  }
 
-commandType(line){
+  commandType(line) {
     
-    if (line.charAt(0) ==='@'){
-        return 'A';}
-    if (line.charAt(0)==='('){
-        return 'L';}
-    return 'C'
-}
+    if (line.charAt(0) === '@') {
+      return 'A'; 
+    }
+    if (line.charAt(0) === '(') {
+      return 'L'; 
+    }
+    return 'C';
+  }
 
-// an 'A' command in assembly is the '@' symbol followed by a positive integer,
-// or by a symbol. The symbol may be built-in or user-defined:
-// @512
-// @1337
-// @SCREEN
-// @myVariable
+  // an 'A' command in assembly is the '@' symbol followed by a positive integer,
+  // or by a symbol. The symbol may be built-in or user-defined:
+  // @512
+  // @1337
+  // @SCREEN
+  // @myVariable
 
-aCommand(linearr){
-    var line = linearr[0]
+  aCommand(linearr) {
+    var line = linearr[0];
     var number;
     // is the first character after the @ a digit? if not we have a symbolic address
-    if('0123456789'.indexOf( line.charAt(1) ) !== -1){
-        //we have an integer
-        number = parseInt(line.slice(1));
-    }
-    else{
-        //fetch the address of the symbol
-        var symbol = line.slice(1);
-        number = this.getAddress(symbol);
+    if ('0123456789'.indexOf( line.charAt(1) ) !== -1) {
+      //we have an integer
+      number = parseInt(line.slice(1));
+    } else {
+      //fetch the address of the symbol
+      var symbol = line.slice(1);
+      number = this.getAddress(symbol);
     }
     
     //convert number to binary
     var command = number.toString(2);
     
     //pad to 16 bits
-    while(command.length<16){
-        command = '0'+command;
+    while (command.length < 16) {
+      command = '0' + command;
     }
     
     return command;
     
-}
+  }
 
 
-// a 'C' command must be of the form:
-// DST=CMD for computation commands
-// or
-// DST;JMP for jump commands
-// where DST,CMD, and JMP are assembly commands in the dictionaries dst,cmd,jmp
-cCommand(linearr){
+  // a 'C' command must be of the form:
+  // DST=CMD for computation commands
+  // or
+  // DST;JMP for jump commands
+  // where DST,CMD, and JMP are assembly commands in the dictionaries dst,cmd,jmp
+  cCommand(linearr) {
     
-    var line = linearr[0]
+    var line = linearr[0];
     
     // is the line a computation command?
-    if(line.indexOf('=')!= -1){
-        var dst = line.split('=')[0];
-        var cmd = line.split('=')[1];
-        if(this.dst[dst] && this.cmd[cmd]){
-            return '111'+this.cmd[cmd]+this.dst[dst]+'000';
-        }
+    if (line.indexOf('=') != -1) {
+      var dst = line.split('=')[0];
+      var cmd = line.split('=')[1];
+      if (this.dst[dst] && this.cmd[cmd]) {
+        return '111' + this.cmd[cmd] + this.dst[dst] + '000';
+      }
     }
     // is the line a jump command?
-    else if(line.indexOf(';')!= -1){
-        var cmd = line.split(';')[0];
-        var jmp = line.split(';')[1];
-        if(this.cmd[cmd] && this.jmp[jmp]){
-            return '111'+this.cmd[cmd]+'000'+this.jmp[jmp];
-        }
+    else if (line.indexOf(';') != -1) {
+      var cmd = line.split(';')[0];
+      var jmp = line.split(';')[1];
+      if (this.cmd[cmd] && this.jmp[jmp]) {
+        return '111' + this.cmd[cmd] + '000' + this.jmp[jmp];
+      }
         
     }
     
     //line is not built correcly, throw error
-        throw('Line: '+linearr[1]+', invalid command: '+line);
-}
+    throw ('Line: ' + linearr[1] + ', invalid command: ' + line);
+  }
     
 
 
     
 
-// returns the address of the symbol requested.
-// if the symbol is not already in the dict of symbols,
-// create a new entry beginning at address 16
-getAddress(symbol){
+  // returns the address of the symbol requested.
+  // if the symbol is not already in the dict of symbols,
+  // create a new entry beginning at address 16
+  getAddress(symbol) {
     
-    if(!this.symbolDict[symbol]){
-        this.symbolDict[symbol] = 16+this.userDefVarsCount;
-        this.userDefVarsCount+=1;
+    if (!this.symbolDict[symbol]) {
+      this.symbolDict[symbol] = 16 + this.userDefVarsCount;
+      this.userDefVarsCount += 1;
     }
     return this.symbolDict[symbol];
     
-}
+  }
 
 
 }
@@ -280,14 +279,13 @@ getAddress(symbol){
 // {error: null/'message', lines: []}
 // 
 var assemble = text => {
-    var a = new Assembler();
-    var solution = { error: null, machineCode: [] };
-    try {
-        a.process(text);
-    } catch (err) {
-        solution.error = err;
-    }
-    console.log(a,solution)
-    solution.machineCode = a.machineCode;
-    return solution;
+  var a = new Assembler();
+  var solution = { error: null, machineCode: [] };
+  try {
+    a.process(text);
+  } catch (err) {
+    solution.error = err;
+  }
+  solution.machineCode = a.machineCode;
+  return solution;
 };
